65a285ad0bc99e87e0ae183e3cea685e8485c318
ianotee <87940363+ianotee@users.noreply.github.com>
2022-01-25 15:18:24 +0300

    Add files via upload

---
+++ TRAINING/script.js
@@ -0,0 +1,75 @@
+  // for loop
+ let name = ["Ian", "Otee", "Enyakai", "Movis"];
+
+ for(let i=0;   i<name.length;   i++){
+ 	console.log(name[i]);
+ }
+
+
+// while loop
+let i = 0;
+ while (i<name.length){
+
+      console.log(name[i]);
+
+      i++
+
+ }
+
+
+
+// if statements
+
+ const age = 25;
+
+ if (age>20){
+
+
+ 	console .log('The person is of right age');
+
+ }
+
+
+ //when using to create a password
+
+ const password= "Ot";
+
+ if(password.length>12){
+
+ 	console.log("The password is very mighty");
+
+ }
+  else if(password.length>8){
+
+  console.log("The password is  strong");
+
+ } else{
+   console.log("The password is very weak");
+
+
+ }
+
+
+
+const speak = function (){
+	 console.log("Hello world");
+
+}
+
+
+ speak();
+
+
+ //blog 
+
+ const Title = "The Javascript  book";
+ const Author = " Ian";
+ const Year = " 2022";
+ const likes = 60;
+
+ let results=  "The book  Called"  +" " +Title +" " +"written by" + " " + Author + " " +"That was written in" + 2022 + " had the following" + likes + "likes";
+
+ console.log(results);
+
+
+
